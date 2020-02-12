832cd75a59a88e9ee57ea7e09b8a527c71595e60
PJCHENG <paulcherng@hotmail.com>
2020-02-12 17:59:50 +0800

    CPJ Routing test

--- react-v1.0/src/apps/CPJ/CPJ.js
+++ react-v1.0/src/apps/CPJ/CPJ.js
@@ -1,12 +1,51 @@
 import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+import banner from "./img/YNAIv6n.png"
+import "./css/CPJ.css"
+const Author = "/CPJ/"
 
-class CPJ extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>This CPJ's Page</h1>
+
+class CPJ extends React.Component {
+    render() {
+        return (
+            <div class="body">
+                <img class="banner" src={banner} alt=""></img>
+                <div><App_Link name="App1" /></div>
+                <Test />
             </div>
         )
     }
+}
+class App_Link extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Link to={Author + this.props.name}>{this.props.name}</Link>
+
+                </div>
+
+                <Switch>
+                    <Route path={Author + this.props.name}>
+                        <div>12345</div>
+                    </Route>
+                </Switch>
+            </Router>
+
+        )
+    }
+}
+function Test() {
+    return (
+        <div>
+            <a href="http://localhost:3000/api">test</a>
+        </div>
+    )
 }
 export default CPJ
--- react-v1.0/src/index.js
+++ react-v1.0/src/index.js
@@ -7,28 +7,18 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    Redirect
 } from "react-router-dom";
 import './styles.scss';
 
 const Index = () => {
     return (
         <Router>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                 <a className="navbar-brand" href="#">
                     Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+          </a>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         {/*
@@ -50,6 +40,7 @@ const Index = () => {
                     </ul>
                 </div>
             </nav>
+            <Redirect></Redirect>
             <Switch>
                 {/*
 
